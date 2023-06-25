ee2a2e17ee1f4ece9c90675c8db7299d13e4512d
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:35:37 +0200

    Add class Series which inherits abstract class VideoContent

---
+++ streaming-service-by-class-diagram/Series.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streaming_service_by_class_diagram
+{
+    internal class Series : VideoContent
+    {
+        public List<Season> SeasonsList { get; set; }
+
+        public Series(string name, string description) :base(name, description) {}
+
+        public override string getInfo()
+        {
+            string info = "Series: " + Name + ", number of seasons: " + SeasonsList.Count() + "\n";
+
+            foreach (var season in SeasonsList)
+            {
+                info += "Sezone: " + season.SerialNumber + ", number of episodes: " + season.EpisodeList.Count() + "\n";
+            }
+
+            return info;
+        }
+    }
+}
