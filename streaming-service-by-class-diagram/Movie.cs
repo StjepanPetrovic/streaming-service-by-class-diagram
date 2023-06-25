3fae941d62994de741fa3fda45f900ba382db8d7
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:17:01 +0200

    Add class Movie which inherits abstract class VideoContent

---
+++ streaming-service-by-class-diagram/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streaming_service_by_class_diagram
+{
+    internal class Movie : VideoContent
+    {
+        public int Duration { get; set; }
+
+        public Movie(string name, string description, int duration) :base(name, description)
+        {
+            Duration = duration;
+        }
+
+        public override string getInfo()
+        {
+            return Name + ", " + Desctription + ", " + Duration + " min";
+        }
+    }
+}
