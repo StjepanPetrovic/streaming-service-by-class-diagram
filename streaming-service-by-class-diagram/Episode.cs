c33c5f0d658d789c35546e96aebb5092d31f1e7d
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:19:18 +0200

    Add class Episode

---
+++ streaming-service-by-class-diagram/Episode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streaming_service_by_class_diagram
+{
+    internal class Episode
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
