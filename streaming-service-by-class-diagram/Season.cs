f3b99630bfe3fd7c9d9b7b394a70c22332b6d969
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:23:26 +0200

    Add Season class

---
+++ streaming-service-by-class-diagram/Season.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streaming_service_by_class_diagram
+{
+    internal class Season
+    {
+        public int SerialNumber { get; set; }
+        public List<Episode> EpisodeList { get; set; } = new List<Episode>();
+    }
+}
