6bbd214bd534e73097169049364e3065b3b25314
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:04:48 +0200

    Add abstract class VideoContent

---
+++ streaming-service-by-class-diagram/VideoContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace streaming_service_by_class_diagram
+{
+    internal abstract class VideoContent
+    {
+        public string Name { get; set; }
+        public string Desctription { get; set; }
+        public ContentType Type { get; set; }
+
+        public VideoContent(string name, string desctription)
+        {
+            Name = name;
+            Desctription = desctription;
+        }
+
+        public abstract string getInfo();
+    }
+}
